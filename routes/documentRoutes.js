f43960421048d181d471b77011825d9e053e9c7c
TVNL CHARAN <vnlcharan.t@gmail.com>
2024-06-26 21:36:06 +0530

    added uploadDoc

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -12,12 +12,9 @@ const authAdmin = require("../middlewares/authAdmin");
 
 const router = express.Router();
 
-<<<<<<< HEAD
 router.get("/folder", getDocumentsByFolder);
 router.post("/upload", uploadDocument);
-=======
 router.post("/folder", getDocumentsByFolder);
->>>>>>> refs/remotes/origin/master
 router.get("/", getDocumentById);
 router.put("/", updateDocument);
 router.delete("/", deleteDocument);
