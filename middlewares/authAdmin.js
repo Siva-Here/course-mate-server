5eabd0ac3eb853b8b1d8ee395501cd124c9df1c4
siva <sivahere9484@gmail.com>
2024-07-06 04:19:05 +0530

    solved error admin

--- middlewares/authAdmin.js
+++ middlewares/authAdmin.js
@@ -1,23 +1,53 @@
-const {jwtDecode} = require('jwt-decode');
+// const {jwtDecode} = require('jwt-decode');
+// require('dotenv').config();
+// const adminEmails = process.env.ADMIN_EMAILS.split(',');
+
+// const verifyToken = (req, res, next) => {
+//   next();
+//   const bearerHeader = req.headers['authorization'];
+
+//   if (typeof bearerHeader !== 'undefined') {
+//     const bearerToken = bearerHeader.split(' ')[1];
+//     const decodedToken = jwtDecode(bearerToken);
+//     const userEmail = decodedToken.email;
+//     if (adminEmails.includes(userEmail)) {
+//         req.userEmail = userEmail;
+//         next();
+//     } else {
+//         res.status(401).json({ error: 'Unauthorized: Email not allowed' });
+//     }
+//   } else {
+//     res.sendStatus(401); 
+//   }
+// };
+
+// module.exports = verifyToken;
+
+const jwtDecode = require('jwt-decode');
 require('dotenv').config();
 const adminEmails = process.env.ADMIN_EMAILS.split(',');
 
 const verifyToken = (req, res, next) => {
-  next();
   const bearerHeader = req.headers['authorization'];
 
   if (typeof bearerHeader !== 'undefined') {
     const bearerToken = bearerHeader.split(' ')[1];
-    const decodedToken = jwtDecode(bearerToken);
+    let decodedToken;
+    try {
+      decodedToken = jwtDecode(bearerToken);
+    } catch (error) {
+      return res.status(400).json({ error: 'Invalid token' });
+    }
+
     const userEmail = decodedToken.email;
     if (adminEmails.includes(userEmail)) {
-        req.userEmail = userEmail;
-        next();
+      req.userEmail = userEmail;
+      next();
     } else {
-        res.status(401).json({ error: 'Unauthorized: Email not allowed' });
+      return res.status(401).json({ error: 'Unauthorized: Email not allowed' });
     }
   } else {
-    res.sendStatus(401); 
+    return res.sendStatus(401); // Unauthorized
   }
 };
 
